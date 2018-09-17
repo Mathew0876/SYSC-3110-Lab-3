e2822804c43177294769eb626bc0db9f9e5e007f
Mathew <Mathew@192.168.0.56>
2018-09-17 19:11:51 -0400

    Change 1

---
+++ src/AddressBook.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * Mathew Smith
+ * 101029870
+ * SYSC 3110A L1
+ *
+ */
+public class AddressBook {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("AddressBook.java");
+
+	}
+	
+	public BuddyInfo addBuddy(String name) {
+		return new BuddyInfo(name);
+		
+	}
+	
+	public BuddyInfo removeBuddy(String name) {
+		return new BuddyInfo(name);
+		
+	}
+	
+	
+
+}
